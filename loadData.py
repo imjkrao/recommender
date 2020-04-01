c404bac3017afd68bf3a9236d0030316481ab541
Jeevankrishna <raojk33@gmail.com>
2020-04-01 18:53:16 +0100

    Recommender
    initial commit -- load data

--- loadData.py
+++ loadData.py
@@ -8,8 +8,10 @@ Created on Wed Apr  1 17:34:52 2020
 import pandas as pd
 
 import easygui
+
 U_filename=easygui.fileopenbox(title="Select User data")
 userDataset= pd.read_csv(U_filename)
 
 J_filename=easygui.fileopenbox(title="Select Job data")
 jobDataset= pd.read_csv(J_filename, encoding='unicode_escape')
+
